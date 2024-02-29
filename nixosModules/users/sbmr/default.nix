8e4a964ee8d9f8493e81891fa66d3f576495e8ed
mraethel <mraethel@outlook.com>
2024-02-29 17:46:45 +0100

    sbmr

--- nixosModules/users/sbmr/default.nix
+++ nixosModules/users/sbmr/default.nix
@@ -1,9 +1,18 @@
 {
+  config,
   mraethel,
   ...
 }: {
-  users.users.sbmr.isNormalUser = true;
+  users.users.sbmr = {
+    isNormalUser = true;
+    extraGroups = [ "keys" ];
+  };
   home-manager.users.sbmr.imports = (with mraethel.homeModules.config; [
     git
   ]) ++ [ mraethel.homeModules.users.sbmr ];
+  sops.secrets."sbmr/pem" = {
+    sopsFile = ../../../secrets/users/sbmr.yaml;
+    mode = "0400";
+    owner = config.users.users.sbmr.name;
+  };
 }
