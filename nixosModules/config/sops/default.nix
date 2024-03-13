1dc42992475ef7a476020cc83575b0f90159af78
mraethel <mraethel@outlook.com>
2024-03-13 11:13:54 +0100

    sops (refactor)

--- nixosModules/config/sops/default.nix
+++ nixosModules/config/sops/default.nix
@@ -1,12 +1,14 @@
-{
+let
+  sshKey = rec {
+    path = "/etc/ssh/ssh_sops_${ type }_key";
+    type = "ed25519";
+  };
+in {
   sops = {
     defaultSopsFile = ../../../secrets/secrets.yaml;
-    age.sshKeyPaths = [ "/etc/ssh/ssh_sops_ed25519_key" ];
+    age.sshKeyPaths = [ sshKey.path ];
     secrets.github_pat = { };
   };
-  services.openssh.hostKeys = [{
-    path = "/etc/ssh/ssh_sops_ed25519_key";
-    type = "ed25519";
-  }];
+  services.openssh.hostKeys = [{ inherit (sshKey) path type; }];
   programs.sops.enable = true;
 }
