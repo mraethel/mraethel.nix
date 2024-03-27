8963a268d54eade471a80a55274981fc24e5e63b
mraethel <mraethel@outlook.com>
2024-03-27 02:30:39 +0100

    flake (update) (glirc)

--- legacyPackages/haskellPackages/glirc/default.nix
+++ legacyPackages/haskellPackages/glirc/default.nix
@@ -3,17 +3,7 @@
   haskell
 }: haskellPackages.override {
   overrides = self: super: {
-    glirc = super.callHackageDirect {
-      pkg = "glirc";
-      ver = "2.40.1";
-      sha256 = "sha256-iVnzuiekKCMi/PLUiOMnmqJWn9T/KKbH3PJ1rkrTQbs=";
-    } { };
-    irc-core = super.callHackageDirect {
-      pkg = "irc-core";
-      ver = "2.12";
-      sha256 = "sha256-emQt7X5cpvv+gidN5Ueqj392efICMOxIB9Wcoz9K4RU=";
-    } { };
-    vty = super.vty_6_1;
-    vty-unix = haskell.lib.markUnbroken super.vty-unix;
+    vty_5_35_1 = super.vty;
+    irc-core = haskell.lib.markUnbroken super.irc-core;
   };
 }
