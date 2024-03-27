6c9abb33d44fd3cb911b419b3b37ee7cba6e29d5
mraethel <mraethel@outlook.com>
2024-03-27 23:53:00 +0100

    chromium (bookmarks)

--- nixosModules/options/ungoogled-chromium/default.nix
+++ nixosModules/options/ungoogled-chromium/default.nix
@@ -6,13 +6,41 @@
   ...
 }: let 
   cfg = config.programs.chromium;
+  mbType = with lib.types; listOf (attrsOf (either str mbType));
+# Maybe one day this will work (https://github.com/NixOS/nixpkgs/pull/284551)
+# mbType = with lib.types;
+#   listOf
+#     (oneOf [
+#       (submodule { options.toplevel-name = lib.mkOption { type = str; }; })
+#       (submodule {
+#         options = {
+#           name = lib.mkOption { type = str; };
+#           url = lib.mkOption { type = str; };
+#         };
+#       })
+#       (submodule {
+#         options = {
+#           name = lib.mkOption { type = str; };
+#           children = lib.mkOption { type = mbType; };
+#         };
+#       })
+#     ]);
 in {
   imports = [
     ./privoxy
     ./searx
   ];
 
-  options.programs.chromium.package = lib.mkPackageOption mraethel.packages.${ system } "ungoogled-chromium" { };
+  options.programs.chromium = {
+    package = lib.mkPackageOption mraethel.packages.${ system } "ungoogled-chromium" { };
+    ManagedBookmarks = lib.mkOption {
+      type = mbType;
+      default = [ ];
+    };
+  };
 
-  config.environment.systemPackages = lib.mkIf cfg.enable [ cfg.package ];
+  config = lib.mkIf cfg.enable {
+    environment.systemPackages = [ cfg.package ];
+    programs.chromium.extraOpts.ManagedBookmarks = cfg.ManagedBookmarks;
+  };
 }
