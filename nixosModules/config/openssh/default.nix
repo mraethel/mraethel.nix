df1e258bf5bc96a93e55a76f4286cc599f750d8f
mraethel <mraethel@outlook.com>
2024-08-01 01:02:22 +0200

    openssh (pki)

--- nixosModules/config/openssh/default.nix
+++ nixosModules/config/openssh/default.nix
@@ -5,5 +5,9 @@
       path = "/etc/ssh/ssh_host_ed25519_key";
       type = "ed25519";
     }];
+    settings = {
+      PasswordAuthentication = false;
+      KbdInteractiveAuthentication = false;
+    };
   };
 }
