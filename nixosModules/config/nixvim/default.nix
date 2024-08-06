c67c6fe6adbc5e356a5b112d741b191968a3f399
mraethel <mraethel@outlook.com>
2024-08-06 22:25:21 +0200

    nixvim (mapping)

--- nixosModules/config/nixvim/default.nix
+++ nixosModules/config/nixvim/default.nix
@@ -59,6 +59,15 @@
               option.get_bufnrs.__raw = "vim.api.nvim_list_bufs";
             }
           ];
+          mapping = {
+            "<C-Space>" = "cmp.mapping.complete()";
+            "<C-d>" = "cmp.mapping.scroll_docs(-4)";
+            "<C-e>" = "cmp.mapping.close()";
+            "<C-f>" = "cmp.mapping.scroll_docs(4)";
+            "<CR>" = "cmp.mapping.confirm({ select = true })";
+            "<S-Tab>" = "cmp.mapping(cmp.mapping.select_prev_item(), {'i', 's'})";
+            "<Tab>" = "cmp.mapping(cmp.mapping.select_next_item(), {'i', 's'})";
+          };
           snippet.expand = ''
             function(args)
               require('luasnip').lsp_expand(args.body)
