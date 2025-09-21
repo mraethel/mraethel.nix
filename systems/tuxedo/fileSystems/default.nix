c22d71cdfc0ada7287476ece61ce038149be3afb
mraethel <mraethel@outlook.com>
2025-09-21 18:02:05 +0000

    nvme (update)

--- systems/tuxedo/fileSystems/default.nix
+++ systems/tuxedo/fileSystems/default.nix
@@ -1,10 +1,11 @@
 {
   "/" = {
-    device = "/dev/disk/by-uuid/abbe5717-8477-4bf6-9240-0ad8186bc268";
+    device = "/dev/disk/by-uuid/37b5eb0a-aec1-4836-b3fd-0f27bdd010c8";
     fsType = "ext4";
   };
   "/boot" = {
-    device = "/dev/disk/by-uuid/3F18-D96A";
+    device = "/dev/disk/by-uuid/C6DC-F881";
     fsType = "vfat";
+    options = [ "fmask=0022" "dmask=0022" ];
   };
 }
--- systems/tuxedo/luksDevices/default.nix
+++ systems/tuxedo/luksDevices/default.nix
@@ -1,1 +1,1 @@
-{ cryptroot.device = "/dev/disk/by-uuid/33f12d05-280b-4e5e-93d8-0ecc63b7f99c"; }
+{ cryptroot.device = "/dev/disk/by-uuid/488865bd-36be-4609-84ea-ec45c568c540"; }
