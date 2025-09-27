e08b71de7b3fe87c425a7f69578cd748fd3e050e
mraethel <mraethel@outlook.com>
2025-09-28 00:52:34 +0200

    networkd (enable DHCP)

--- nixosModules/config/networking/default.nix
+++ nixosModules/config/networking/default.nix
@@ -1,5 +1,11 @@
 {
   networking.useDHCP = false;
   services.resolved.enable = true;
-  systemd.network.enable = true;
+  systemd.network = {
+    enable = true;
+    networks.wlan0 = {
+      name = "wlan0";
+      DHCP = "yes";
+    };
+  };
 }
